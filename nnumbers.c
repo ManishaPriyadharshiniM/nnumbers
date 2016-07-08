47fd26c3a0ba2d4aa68c58c349781a306de6058d
ManishaPriyadharshiniM <manisha96priya@gmail.com>
2016-07-08 15:34:53 +0530

    finish

--- nnumbers.c
+++ nnumbers.c
@@ -3,13 +3,11 @@
 #include<conio.h>
 void main()
 {
-int a,b,c,s=0;
+int a,c,s=0;
 printf("enter the digits to add\n");
 scanf("%d",&a);
-printf("enter the no of digits",a);
 for(c=0;c<=a;c++);
 {
-scanf("%d",&b);
 s=s+b;
 }
 printf("sum is=\n",s);
